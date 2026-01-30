6cd4f5d4bca403548bc189f53f163939ab09ab7f
UchitChavdaMph1 <uchit.chavda1@mphasis.com>
2026-01-30 09:59:50 +0530

    Bootstrap examples added

--- SpringBootRestApi_JPA/src/main/java/com/test/controller/StudentController.java
+++ SpringBootRestApi_JPA/src/main/java/com/test/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/student/v1/api")
+@CrossOrigin(origins = "http://localhost:4200")
 @Tag(name = "Student Api", description = "Student CRUDoperations with JPA")
 public class StudentController {
 	
@@ -62,7 +64,7 @@ public class StudentController {
 		}
 	}
 	
-	@GetMapping(value="/{id}")
+	@GetMapping(value="getbyId/{id}")
 	@Operation(method = "getStudentById" ,description = "get student from db by id")
 	public ResponseEntity<Optional<Student>> getStudentById(@PathVariable int id) {
 		Optional<Student> obj = service.getStudentById(id);
@@ -74,7 +76,7 @@ public class StudentController {
 	}
 	
 	
-	@PutMapping(value="/{id}")
+	@PutMapping(value="updateById/{id}")
 	@Operation(method = "updateStudentById" ,description = "update student data in DB")
 	public ResponseEntity<Student> updateStudentById(@PathVariable int id ,@RequestBody Student std) {
 		std.setId(id);
@@ -99,7 +101,7 @@ public class StudentController {
 	}
 	
 	 
-	@GetMapping(value="/list")
+	@GetMapping(value="/get")
 	@Operation(method = "getAllStudents" ,description = "get all students from DB")
 	public ResponseEntity<List<Student>> getAllStudent() {
 		List<Student> obj = service.getAllStudents();
